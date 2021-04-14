32da5b121291f6cc3b09d9846075eb6381f2876a
albannablade123 <hasanal082000@gmail.com>
2021-04-15 01:48:02 +0300

    First Commit

---
+++ Documents/e-kayseri/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthPageComponent } from './auth-page/auth-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
